0f9dbfdcc0a23c591c3740f244f6cc6e8ab9dfd6
taptaplit <taptaplit@pop-os.localdomain>
2021-12-19 18:24:55 -0500

    started V 1.0.1

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express")
+const request = require("request")
 const app = express()
 const port = 3000 || process.ENV.PORT
 
 
 
-app.use(express.static(__dirname));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.redirect("/v-1")
 })
 
-app.get("/v-1", (req, res) => {
-    res.sendFile(__dirname + "/v-1/index.html");
+app.get("/v-1", async (req, res) => {
+    request({url: 'https://portfolio-backend-mongo-server.herokuapp.com/about/skill-content', json: true}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.render('v-1/pages/index', {skills: body.result});
+        }
+    })
 })
 
 app.listen(port, () => {
